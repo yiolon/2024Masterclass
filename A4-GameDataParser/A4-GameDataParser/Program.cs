4d7abe1d239cad502edfacca6a828a3252f0787b
yiolon <hanhx777@gmail.com>
2024-08-10 21:13:05 +0800

    finish check user input file path method

--- A4-GameDataParser/A4-GameDataParser/Program.cs
+++ A4-GameDataParser/A4-GameDataParser/Program.cs
@@ -1,29 +1,62 @@
 ﻿
-namespace bodypart
+
+class main
 {
-    class main
+
+static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            CheckUserPath(); //check the file path inputted by user
+        string filePath = CheckUserPath(); //check the file path inputted by user
 
-            //read data file from path
+        ReadFromPath();//read data file from path
 
-            //Printing video games by serializing json
-        }
+        //Printing video games by serializing json
+    }
 
-        private static void CheckUserPath()
+    public static string CheckUserPath()
+    {
+        string filePath = null;
+
+        while (true) 
         {
             Console.WriteLine("Enter the name of the file you want to read:");
             try
-            {
-                string filePath = Console.ReadLine();
+            {               
+                filePath = Console.ReadLine();
+                if (filePath == null)
+                {
+                    throw new NullReferenceException("File name cannot be null.");
+                }
+                else if (string.IsNullOrEmpty(filePath))
+                {
+                    throw new ArgumentNullException("File name cannot be empty.");
+                }
+                else if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException("File not found.");
+                }
+
+                return filePath;
             }
-            catch (NullReferenceException nullEx)
+
+            catch (NullReferenceException ex)
             {
-                Console.WriteLine("File name cannot be null.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message); 
             }
         }
+
     }
-    
+
+    private static void ReadFromPath()
+    {
+        throw new NotImplementedException();
+    }
 }
+  
