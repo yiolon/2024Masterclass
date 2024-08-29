199eb210d91d2b1c06d36e344acc479c6a1884ff
yiolon <hanhx777@gmail.com>
2024-08-29 14:39:08 +0800

    A6 transform 3 method to LINQ

---
+++ A6-LINQCookieCookbook/DataAccess/StringsTextualRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace CookieCookbook.DataAccess;
+
+public class StringsTextualRepository : StringsRepository
+{
+    private static readonly string Separator = Environment.NewLine;
+
+    protected override string StringsToText(List<string> strings) => string.Join(Separator, strings);
+  
+
+    protected override List<string> TextToStrings(string fileContents)
+    {
+        return fileContents.Split(Separator).ToList();
+    }
+}
